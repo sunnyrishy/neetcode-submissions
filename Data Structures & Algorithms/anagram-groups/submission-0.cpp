4aee73e61aa8b1554a85625a10010b4056bc63b7
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2026-07-28 17:20:53 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mpp;
+        for(int i = 0; i < strs.size(); i++){
+            string key = strs[i];
+            sort(key.begin(), key.end());
+            mpp[key].push_back(strs[i]);
+        }
+        vector<vector<string>> res;
+        for(auto it : mpp){
+            res.push_back(it.second);
+        }
+        return res;
+
+        
+    }
+};
